4041e98f5bde985be0aebc75ec2bc560a8d9091c
bahaa <bahaamagdygit@gmail.com>
2025-11-17 22:47:47 +0200

    add raut linke to use navbar and cards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'section',
+        loadComponent: () => import('../../Layout/genaric-card/min-section/min-section').then(m => m.MinSection)
+    },
+    {
+        path: 'min-genaric-card',
+        loadComponent: () => import('../../Layout/genaric-card/min-genaric-card/min-genaric-card').then(m => m.MinGenaricCard)
+    },
+    {
+        path: '',
+        redirectTo: 'section',
+        pathMatch: 'full'
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NaveBar } from '../../Layout/NavFooter/nave-bar/nave-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NaveBar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
