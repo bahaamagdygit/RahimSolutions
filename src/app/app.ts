e7e4d8b7e2b2784f5fc3caf175ea329395be315b
bahaa <bahaamagdygit@gmail.com>
2025-11-26 22:28:56 +0200

    some inhansed

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,7 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NaveBar } from '../../Layout/NavFooter/nave-bar/nave-bar';
 import { Footer } from '../../Layout/NavFooter/footer/footer';
-import { GenaricSlider } from '../../Layout/slider/genaric-slider/genaric-slider';
-import { TestimonialSlider } from '../../Layout/slider/testimonial-slider/testimonial-slider';
-import { CardsListSection } from './cards/cards-list-section/cards-list-section';
 import { ImgDetails } from "./Profile/img-details/img-details";
-
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, NaveBar, Footer],
