b6d6c8ba192f06ec5671fef5995f8c34b702fbf6
bahaa <bahaamagdygit@gmail.com>
2025-11-29 12:17:48 +0200

    logic flage showNavFooter inisDashboard

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { NaveBar } from '../../Layout/NavFooter/nave-bar/nave-bar';
 import { Footer } from '../../Layout/NavFooter/footer/footer';
-import { ImgDetails } from "./Profile/img-details/img-details";
+import { filter } from 'rxjs/operators';
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, NaveBar, Footer],
@@ -11,4 +12,15 @@ import { ImgDetails } from "./Profile/img-details/img-details";
 })
 export class App {
   protected readonly title = signal('RahimSolutions');
+  protected readonly showNavFooter = signal(true);
+
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      // Hide navbar and footer for dashboard routes
+      const isDashboard = event.urlAfterRedirects.startsWith('/dashboard');
+      this.showNavFooter.set(!isDashboard);
+    });
+  }
 }
